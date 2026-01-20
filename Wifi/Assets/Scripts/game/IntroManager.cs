23431b40d7474622df1463edc680d6f1d2c8f009
Jose Luis <jlrp09102005@gmail.com>
2026-01-20 20:26:50 +0100

    Working Barely

--- Wifi/Assets/Scripts/game/IntroManager.cs
+++ Wifi/Assets/Scripts/game/IntroManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a952c49aacbde3603ced3cca1f4307652e7d5076a6f89c3e3b0036fad8bfaa9
-size 1349
+oid sha256:8cc960043f5b069136a448f6b84359d9d10023334a265cde4f1adc454b2ef2ff
+size 2998
--- Wifi/Assets/Scripts/game/RandomEventManager.cs
+++ Wifi/Assets/Scripts/game/RandomEventManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01aff9a0e50fabd755e9715127208b4577b031b60b16c4fe9828ef5ec54987ae
-size 2223
+oid sha256:9a8a3a4cbadb0111760f396d532e49edfe716801c98808526af0952be00610de
+size 2485
