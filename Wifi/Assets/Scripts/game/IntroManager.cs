a1a5b7e6e5bab95da9da71972018b006b4f2b2d4
Jose Luis <jlrp09102005@gmail.com>
2026-01-20 21:22:12 +0100

    More Cinematics

--- Wifi/Assets/Scripts/game/IntroManager.cs
+++ Wifi/Assets/Scripts/game/IntroManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b250d06d72bc3f1460dd9298ad84e1b9834470632e6614fad2af249d489c287
-size 3369
+oid sha256:ae9344996d32f8127184b864f16dc875fb36ea493e394857731cc039a33bbaac
+size 4705
